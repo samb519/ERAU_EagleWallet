27182b425b26dc4dc25fd5dc91e289453268c2e4
Joseph Moran <44380816+omone1@users.noreply.github.com>
2021-02-23 16:28:02 -0500

    Almost finished Home Page

--- app/src/main/java/com/example/eaglewallet/ui/calender/CalenderFragment.java
+++ app/src/main/java/com/example/eaglewallet/ui/calender/CalenderFragment.java
@@ -23,13 +23,7 @@ public class CalenderFragment extends Fragment {
         calenderViewModel =
                 new ViewModelProvider(this).get(CalenderViewModel.class);
         View root = inflater.inflate(R.layout.fragment_calender, container, false);
-        final TextView textView = root.findViewById(R.id.text_calender);
-        calenderViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 }
