877da85a85b9cfe405e7d2099fc4711935dea825
MikeF98 <fornitomichael@gmail.com>
2021-02-25 01:01:09 -0500

    Cleaning

--- app/src/main/java/com/example/eaglewallet/homePage.java
+++ app/src/main/java/com/example/eaglewallet/homePage.java
@@ -1,6 +1,10 @@
 package com.example.eaglewallet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -12,6 +16,7 @@ import androidx.navigation.ui.NavigationUI;
 
 public class homePage extends AppCompatActivity {
 
+    ImageButton SettingsBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +30,7 @@ public class homePage extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
     }
 
 }
