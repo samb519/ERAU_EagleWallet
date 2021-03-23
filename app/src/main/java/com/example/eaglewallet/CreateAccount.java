c8dae396a456f78d9d504fd7cb33b2b285849d88
Will <edwarw12@my.erau.edu>
2021-03-23 14:59:39 -0400

    Requests

--- app/src/main/java/com/example/eaglewallet/CreateAccount.java
+++ app/src/main/java/com/example/eaglewallet/CreateAccount.java
@@ -4,8 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 
 public class CreateAccount extends AppCompatActivity {
 
@@ -30,10 +48,86 @@ public class CreateAccount extends AppCompatActivity {
         CreatAccountNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(CreateAccount.this, ChoosePayment.class );
-                startActivity(intent);
+                String code = postUserRegistration();
+
+                if (code == "200") {
+                    Intent intent = new Intent(CreateAccount.this, ChoosePayment.class );
+                    startActivity(intent);
+                } else if (code == "401") {
+
+                }
             }
         });
 
+    }
+
+    private String postUserRegistration() {
+        final String[] code = {""};
+
+        EditText usernameText = (EditText) findViewById(R.id.editStudentID);
+        String username = usernameText.getText().toString();
+
+        EditText passwordText = (EditText) findViewById(R.id.editTextTextPassword2);
+        String password = passwordText.getText().toString();
+
+        EditText emailText = (EditText) findViewById(R.id.editTextTextEmailAddress2);
+        String email = emailText.getText().toString();
+
+        Log.i("USERNAME", username);
+        Log.i("PASSWORD", password);
+        Log.i("EMAIL", email);
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String url ="https://eaglewallet.wise-net.xyz/api/Auth/register";
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("username", username);
+            jsonBody.put("password", password);
+            jsonBody.put("email", email);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        final String requestBody = jsonBody.toString();
+
+        StringRequest req = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.i("VOLLEY", response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("VOLLEY", error.toString());
+            }
+        }) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                try {
+                    return requestBody == null ? null : requestBody.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
+                    return null;
+                }
+            }
+
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse res) {
+                String responseCode = "";
+                if (res != null) {
+                    responseCode = String.valueOf(res.statusCode);
+                    code[0] = responseCode;
+                }
+                return Response.success(responseCode, HttpHeaderParser.parseCacheHeaders(res));
+            }
+        };
+
+        queue.add(req);
+        //Temp
+        return code[0];
     }
 }
--- app/src/main/java/com/example/eaglewallet/loginPage.java
+++ app/src/main/java/com/example/eaglewallet/loginPage.java
@@ -3,9 +3,27 @@ package com.example.eaglewallet;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.UnsupportedEncodingException;
 
 public class loginPage extends AppCompatActivity {
 
@@ -19,9 +37,17 @@ public class loginPage extends AppCompatActivity {
 
         loginButton = (Button)findViewById(R.id.loginButton);
         loginButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent intent = new Intent(loginPage.this,homePage.class);
-                startActivity(intent);
+            public void onClick(View v) {
+                String code = postUserLogin();
+
+                if (code == "200") {
+                    Intent intent = new Intent(loginPage.this,homePage.class);
+                    startActivity(intent);
+                } else if (code == "401") {
+                    String failure = "Some failure message";
+                } else { //Realistically only 200 and 401 should occur, everything else is some weird scenario
+                    String failure = "We have no clue what happened";
+                }
             }
         });
 
@@ -29,6 +55,7 @@ public class loginPage extends AppCompatActivity {
         CreateAccountTxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent intent = new Intent(loginPage.this, CreateAccount.class );
                 startActivity(intent);
             }
@@ -36,4 +63,68 @@ public class loginPage extends AppCompatActivity {
 
 
     }
+
+    private String postUserLogin() {
+        final String[] code = {""};
+
+        EditText usernameText = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        String username = usernameText.getText().toString();
+
+        EditText passwordText = (EditText) findViewById(R.id.editTextTextPassword);
+        String password = passwordText.getText().toString();
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String url ="https://eaglewallet.wise-net.xyz/api/Auth/login";
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("username", username);
+            jsonBody.put("password", password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        final String requestBody = jsonBody.toString();
+
+        StringRequest req = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.i("VOLLEY", response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("VOLLEY", error.toString());
+            }
+        }) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                try {
+                    return requestBody == null ? null : requestBody.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
+                    return null;
+                }
+            }
+
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse res) {
+                String responseCode = "";
+                if (res != null) {
+                    responseCode = String.valueOf(res.statusCode);
+                    code[0] = responseCode;
+                }
+                return Response.success(responseCode, HttpHeaderParser.parseCacheHeaders(res));
+            }
+        };
+
+        queue.add(req);
+        //Temp
+//        http://arnab.ch/blog/2013/08/asynchronous-http-requests-in-android-using-volley/
+        return code[0];
+    }
+
 }
