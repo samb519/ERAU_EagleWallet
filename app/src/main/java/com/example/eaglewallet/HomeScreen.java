171e0318c6174e0986d248f899039542fd2102f1
MikeF98 <fornitomichael@gmail.com>
2021-04-08 19:51:42 -0400

    added settings into homescreen

--- app/src/main/java/com/example/eaglewallet/HomeScreen.java
+++ app/src/main/java/com/example/eaglewallet/HomeScreen.java
@@ -12,6 +12,8 @@ import android.widget.TextView;
 public class HomeScreen extends AppCompatActivity {
 
     ImageButton calenderPScreenBtn;
+    ImageButton paymentHScreenBtn;
+    ImageButton settingHScreenBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +21,15 @@ public class HomeScreen extends AppCompatActivity {
 
         calenderPScreenBtn = (ImageButton) findViewById(R.id.calenderHScreenBtn);
         calenderPScreenBtn .setOnClickListener(v ->
-                clickedCalenderBtn( "https://eraudining.sodexomyway.com/dining-near-me/hours"));{ }
+                clickedCalenderBtn( "https://eraudining.sodexomyway.com/dining-near-me/hours"));{}
+
+        paymentHScreenBtn = (ImageButton)findViewById((R.id.paymentHScreenBtn));
+        paymentHScreenBtn .setOnClickListener(v ->
+                clickedPaymentBtn());{}
+
+        settingHScreenBtn = (ImageButton)findViewById((R.id.settingsHScreenBtn));
+        settingHScreenBtn.setOnClickListener(v ->
+                clickedSettingBtn());{}
     }
 
     public void clickedCalenderBtn(String url){
@@ -27,5 +37,17 @@ public class HomeScreen extends AppCompatActivity {
         intent.setData(Uri.parse(url));
         startActivity(intent);
 
+    }
+
+    public  void clickedPaymentBtn()
+    {
+        Intent intent=new Intent(HomeScreen.this, PaymentHomeScreen.class);
+        startActivity(intent);
+    }
+
+    public  void clickedSettingBtn()
+    {
+        Intent intent=new Intent(HomeScreen.this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/eaglewallet/PaymentHomeScreen.java
@@ -0,0 +1,40 @@
+package com.example.eaglewallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+public class PaymentHomeScreen extends AppCompatActivity {
+
+    ImageButton calenderPScreenBtn;
+    ImageButton homePScreenBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_home_screen);
+
+        homePScreenBtn = (ImageButton)findViewById((R.id.homePScreenBtn));
+        homePScreenBtn.setOnClickListener(v ->
+                clickedHomeBtn());{}
+
+        calenderPScreenBtn = (ImageButton) findViewById(R.id.calenderHScreenBtn);
+        calenderPScreenBtn .setOnClickListener(v ->
+                clickedCalenderBtn( "https://eraudining.sodexomyway.com/dining-near-me/hours"));{}
+    }
+
+    public  void clickedHomeBtn()
+    {
+        Intent intent=new Intent(PaymentHomeScreen.this, HomeScreen.class);
+        startActivity(intent);
+    }
+
+    public void clickedCalenderBtn(String url){
+        Intent intent=new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
+}
