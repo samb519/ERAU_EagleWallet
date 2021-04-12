a38564c4bf7a0bc4a4b10c3b02e86717fb4a0745
MikeF98 <fornitomichael@gmail.com>
2021-04-12 01:05:56 -0400

    added add funds screen, cleaned up payment screen

---
+++ app/src/main/java/com/example/eaglewallet/AddPaymentFromPaymentScreen.java
@@ -0,0 +1,14 @@
+package com.example.eaglewallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddPaymentFromPaymentScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_payment_from_payment_screen);
+    }
+}
--- app/src/main/java/com/example/eaglewallet/PaymentHomeScreen.java
+++ app/src/main/java/com/example/eaglewallet/PaymentHomeScreen.java
@@ -5,50 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 
 public class PaymentHomeScreen extends AppCompatActivity {
 
-    ImageButton calenderPScreenBtn;
     ImageButton homePScreenBtn;
-    ProgressBar dinningDollars,sodexo, eagleDollars, mealPlans;
+    Button addFundBtnPScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_home_screen);
 
-        dinningDollars = (ProgressBar)findViewById(R.id.Dinning_DollarsBar);
-        sodexo = (ProgressBar)findViewById(R.id.SodexoBar);
-        eagleDollars = (ProgressBar)findViewById(R.id.Eagle_DollarsBar);
-        mealPlans = (ProgressBar)findViewById(R.id.Meal_PlansBar);
-
-        homePScreenBtn = (ImageButton)findViewById(R.id.homePScreenBtn);
+        addFundBtnPScreen = (Button) findViewById(R.id.addFundBtnPScreen);
+        addFundBtnPScreen.setOnClickListener(v ->
+                clickedAddFundBtn());
+        {
+        }
+        homePScreenBtn = (ImageButton) findViewById(R.id.homePScreenBtn);
         homePScreenBtn.setOnClickListener(v ->
-                clickedHomeBtn());{}
-
-        calenderPScreenBtn = (ImageButton) findViewById(R.id.calenderPScreenBtn);
-        calenderPScreenBtn.setOnClickListener(v ->
-                clickedCalenderBtn("https://eraudining.sodexomyway.com/dining-near-me/hours"));{}
+                clickedHomeBtn());
+        {
+        }
     }
 
-    public  void clickedHomeBtn()
-    {
-        Intent intent=new Intent(PaymentHomeScreen.this, HomeScreen.class);
+    private void clickedAddFundBtn() {
+        Intent intent = new Intent(PaymentHomeScreen.this, AddPaymentFromPaymentScreen.class);
         startActivity(intent);
     }
 
-    public void clickedCalenderBtn(String url)
-    {
-        Intent intent=new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse(url));
+    private void clickedHomeBtn() {
+        Intent intent = new Intent(PaymentHomeScreen.this, HomeScreen.class);
         startActivity(intent);
-    }
-
-    public void setMealProgressBar(ProgressBar bar, int currentMeal, int totalMeal)
-    {
-        int current = (currentMeal/totalMeal)*100;
-        bar.setProgress(current);
     }
 }
