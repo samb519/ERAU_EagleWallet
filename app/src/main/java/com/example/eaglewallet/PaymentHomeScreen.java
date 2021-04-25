c26e18dcc316fd54bd7c358b0ceb241702ec7eaa
MikeF98 <fornitomichael@gmail.com>
2021-04-24 20:21:04 -0400

    finished payment history, settings work in progress

--- app/src/main/java/com/example/eaglewallet/PaymentHomeScreen.java
+++ app/src/main/java/com/example/eaglewallet/PaymentHomeScreen.java
@@ -7,24 +7,51 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PaymentHomeScreen extends AppCompatActivity {
 
     ImageButton homePScreenBtn;
     Button addFundBtnPScreen;
+    LinearLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_home_screen);
 
+        layout = (LinearLayout) findViewById(R.id.historyLinearLayout);
         addFundBtnPScreen = (Button) findViewById(R.id.addFundBtnPScreen);
         addFundBtnPScreen.setOnClickListener(v ->
                 clickedAddFundBtn());{}
         homePScreenBtn = (ImageButton) findViewById(R.id.homePScreenBtn);
         homePScreenBtn.setOnClickListener(v ->
                 clickedHomeBtn());{}
+
+                //MUST REMOVE: Testing purposes
+//        ArrayList<String> test = new ArrayList<>();
+//        test.add("3/22/2021: Payment of Sodexo bought for $200");
+//        test.add("4/22/2021: Payment of Sodexo bought for $200");
+//        createHistoryTable(test);
+    }
+
+    private void createHistoryTable(ArrayList<String> history)
+    {
+        for(int i = 0; i<history.size();i++)
+        {
+            TextView text = new TextView(this);
+            text.setText(history.get(i));
+            text.setTextSize(14);
+            text.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            ((LinearLayout) layout).addView(text);
+        }
     }
 
     private void clickedAddFundBtn() {
--- app/src/main/java/com/example/eaglewallet/SettingsActivity.java
+++ app/src/main/java/com/example/eaglewallet/SettingsActivity.java
@@ -8,64 +8,79 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
-    private Spinner spinner;
-    private static final String[] paths = {"item 1", "item 2", "item 3"};
     Button SettingsSubmit, SettingsBack;
+    EditText name,lastname,email;
+    TextView studentId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-//        spinner = (Spinner)findViewById(R.id.PaymentList);
-//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-//                android.R.layout.simple_spinner_item,paths);
-//
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        spinner.setAdapter(adapter);
-//        spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
-//
-//        SettingsSubmit = (Button) findViewById(R.id.SettingsSubmit);
-//        SettingsSubmit.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //Send the data to the databse
-//                Intent intent = new Intent(SettingsActivity.this, HomeScreen.class);
-//                startActivity(intent);
-//            }
-//        });
-//
-//        SettingsBack = (Button) findViewById(R.id.SettingsBack);
-//        SettingsBack.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(SettingsActivity.this, HomeScreen.class);
-//                startActivity(intent);
-//            }
-//        });
+        studentId = (TextView) findViewById(R.id.studentId_text);
+        name = (EditText) findViewById(R.id.editFirstNameSettings);
+        lastname = (EditText) findViewById(R.id.editLastNameSettings);
+        email = (EditText) findViewById(R.id.editEmailSettings);
 
+        SettingsSubmit = (Button) findViewById(R.id.SettingsSubmit);
+        SettingsSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Send the data to the databse
+                Intent intent = new Intent(SettingsActivity.this, HomeScreen.class);
+                startActivity(intent);
+            }
+        });
+
+        SettingsBack = (Button) findViewById(R.id.SettingsBack);
+        SettingsBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this, HomeScreen.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
-
-        switch (position) {
-            case 0:
-                // Whatever you want to happen when the first item gets selected
-                break;
-            case 1:
-                // Whatever you want to happen when the second item gets selected
-                break;
-            case 2:
-                // Whatever you want to happen when the thrid item gets selected
-                break;
-
-        }
+    //Call these methods to display the information
+    private  void setId(String name)
+    {
+        studentId.setText(name);
     }
 
-    public void onNothingSelected(AdapterView<?> parent) {
-        // TODO Auto-generated method stub
+    private void setFirstName(String name)
+    {
+        this.name.setHint(name);
     }
 
+    private  void setLastName(String name)
+    {
+        lastname.setHint(name);
+    }
+
+    private void setEmail(String name)
+    {
+        email.setHint(name);
+    }
+
+    //Call these methods to add to the database upon submit
+    private  String getFirstName()
+    {
+        return name.getText().toString();
+    }
+
+    private  String getLastName()
+    {
+        return  lastname.getText().toString();
+    }
+
+    private  String getEmail()
+    {
+        return  email.getText().toString();
+    }
 }
